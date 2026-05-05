38888b8980f0255d93448f54f8c06045bde8e419
lc0nte <lee@MacBook-Pro-de-Lee.local>
2026-05-05 18:27:34 +0200

    mitigaciones aplicadas tras semgrep

--- src/app.py
+++ src/app.py
@@ -7,9 +7,9 @@ NO uses este código en producción.
 import os
 import sqlite3
 
-# Vulnerabilidad 1: credenciales hardcodeadas
-SECRET_KEY = "super-secret-key-1234"
-DATABASE_PASSWORD = "P@ssw0rd_prod_2024!"
+# Mitigación 1: credenciales hardcodeadas importadas como variables de entorno
+SECRET_KEY = os.environ.get("API_KEY", "")
+DATABASE_PASSWORD = os.environ.get("DB_PASSWORD", "")
 
 
 def search_user(username):
@@ -17,17 +17,18 @@ def search_user(username):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
 
-    # Vulnerabilidad 2: SQL Injection — string concatenado directamente en execute
-    cursor.execute("SELECT * FROM users WHERE name = '" + username + "'")
-
+    # Mitigación 2: SQL Injection — string no concatenado directamente
+    query = "SELECT * FROM users WHERE name = ?"
+    cursor.execute(query, (username,))
     return cursor.fetchall()
 
 
 def ping_host(host):
     """Comprueba si un host es accesible."""
     # Vulnerabilidad 3: Command Injection — os.system con input del usuario
-    os.system("ping -c 1 " + host)
-
+    # os.system("ping -c 1 " + host)
+    import subprocess
+    subprocess.run(["ping", "-c" "1", host] check=True)
 
 def main():
     print("App started")
